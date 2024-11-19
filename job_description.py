b4f784b43e80295e0d377c8aba0b5abbe020aae1
Chitraksha13 <chitraksh.kashyap@gmail.com>
2024-11-19 11:04:41 -0500

    Add job_description.py

---
+++ job_description.py
@@ -0,0 +1,58 @@
+job_description = """About the job
+CTC is a cutting-edge proprietary trading firm with a long-term vision and a clear focus on helping the world price and manage risk. Our fun and trusting culture inspires us to solve the industry’s most challenging problems and take calculated risks in a collaborative environment.
+
+We strive to be the most innovative firm in the industry today, tomorrow, and long into the future while upholding ethical excellence. We believe that CTC makes a positive impact on the markets, the lives of our employees, and all the communities to which we belong. Started in 1995 by a team of forward-thinking Traders, we are proud to call ourselves an industry leader that keeps making markets and each other better.
+
+Are you excited by the opportunity to be part of a dynamic trading environment that values teamwork and collaboration? Do you thrive in an innovative atmosphere? If so, we encourage you to consider Chicago Trading Company (CTC)!
+
+The Role 
+
+As a Quant Trading Associate (QTA) Intern, you will be challenged to learn and adapt in an exciting team environment and will play a substantial role in our day-to-day trading and quant-related activities. Your impact is immediate and meaningful. You will become a member of a team for the summer and play a vital role completing project work and/or identifying trading opportunities and communicating with software engineers, quants, traders, and risk managers throughout the trading day. As part of the Summer Associate cohort, you will learn and socialize alongside other QTA Interns and Software Engineering Interns.
+
+What To Expect
+
+The internship program gives you insight into our culture and an inward look into what our business is all about. You will participate in three classroom learning experiences including: a week-long Basics of Options class, a multi-week class on the fundamentals of market making (Mock Trading), and a multi-week class that introduces elements of our quant framework (Quant Curriculum). You will also attend planned social activities and talks from various business leaders to propel your professional growth, and build your network with CTC employees and fellow interns. You will:
+
+Work in an impactful role mirroring that of a full-time Quantitative Trading Associate 
+Get hands-on experience with trading and exposure to our trading technology 
+Have weekly one-on-one meetings with your manager to ensure you are progressing and learning throughout the program 
+Receive mentorship from members of our trading and/or quant teams 
+Experience a culture with a strong emphasis on teamwork and collaboration built through various educational and social activities 
+
+What It Takes For You To Be Successful
+
+You have a competitive drive and are a highly motivated individual that only gets stronger as part of a collaborative team 
+You have a desire to work in tandem with traders, quants, developers, and other experts to further CTC’s goal of providing world class liquidity to financial markets 
+You love solving complex problems 
+You’re intellectually curious and like to challenge the status quo 
+You can effectively communicate sophisticated ideas both written and verbally 
+You have strong quantitative and analytical skills 
+You have basic programming skills or have the desire to learn Python, Java, or C++ 
+No prior experience in finance is necessary, but a deep curiosity and hunger to learn about finance and trading is crucial 
+
+Key Requirements
+
+You are pursuing a Bachelor’s or Master’s degree. All majors and concentrations welcome. 
+You will graduate between December 2025-June 2026 
+You have a demonstrated record of success (academics, awards, etc.) 
+You are willing to relocate to Chicago, IL for the summer (full time placement could be in Chicago, NY or London) 
+You are either eligible to work in the United States with no restrictions or you are eligible to work in the United States temporarily/will require visa sponsorship for permanent authorization 
+
+Why should you consider interning at CTC?
+
+Be part of a fast-growing, industry-leading global technology company 
+Opportunity to work with and learn from leaders in our industry 
+Excellent support system to ensure your learning and development 
+Experience innovation, teamwork, and fun while working in an intellectually stimulating environment 
+Competitive pay, free housing downtown Chicago, and free breakfast, lunch, and healthy snacks! 
+
+Application Timeline
+
+Following the application process, you may receive an online behavioral and cognitive aptitude assessment. 
+Based on your assessment results, you may be invited to a virtual interview. We will contact you directly if we would like to move you to the next round no later than mid-November 2024. 
+
+Our Commitment to Diversity, Equity and Inclusion
+
+At CTC, we aim to cultivate a workplace that celebrates diversity and each person feels included, engaged and empowered. Where each of us feels we belong. We are committed to having a diverse workforce and are proud to be an equal opportunity employer. CTC does not discriminate on the basis of race, religion, color, national origin, sex, gender, gender expression, sexual orientation, age, marital status, veteran status, or disability status. We will ensure that individuals with disabilities are provided reasonable accommodation to participate in the job application or interview process, to perform crucial job functions, and to receive other benefits and privileges of employment.
+
+If you have a disability and believe you need a reasonable accommodation in order to search for a job opening or to apply for a position, please contact us at info@chicagotrading.com. Note that emails sent to this email account for non-disability related issues, such as following up on an application, will not receive a response."""
